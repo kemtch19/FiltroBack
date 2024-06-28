8cfc3da76f61f54954e4720630acefb622e80bec
kemtch19 <kemtch19@gmail.com>
2024-06-27 21:22:10 -0500

    feat: cambios finales

--- obj/Debug/net8.0/FiltroBack.AssemblyInfo.cs
+++ obj/Debug/net8.0/FiltroBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FiltroBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1ca8f7f9871f60072c7bea17d0adb25ade9d97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4935b4655c46ed92f0129025283f3176479e20")]
 [assembly: System.Reflection.AssemblyProductAttribute("FiltroBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FiltroBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
